370e65c9e6be5163827af98c5fcf9031484ee5d2
FRICKO Oliver <fricko@iiasa.ac.at>
2018-09-05 14:25:18 +0200

    added funciton to call macro init

--- tests/ci/make_scenario_db.py
+++ tests/ci/make_scenario_db.py
@@ -2,6 +2,7 @@ import yaml
 
 import ixmp
 import message_ix
+from message_ix.macro import _init_macro
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
         for name, data in yaml.load(f).items():
             scen = message_ix.Scenario(
                 mp, data['model'], data['scenario'], version='new')
+            _init_macro(scen)
             scen.read_excel(name + '.xlsx')
             scen.commit('saving')
 
