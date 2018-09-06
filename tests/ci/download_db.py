79fd6f51a4194fb9b4055908ffb21a2bdcfc9401
Matthew Gidden <matthew.gidden@gmail.com>
2018-09-06 16:47:31 +0200

    print scenarios

--- tests/ci/download_db.py
+++ tests/ci/download_db.py
@@ -2,14 +2,16 @@ import os
 import requests
 import tarfile
 
+import ixmp
+
+from paths import dbpath
+
 username = os.environ['MESSAGE_IX_CI_USER']
 password = os.environ['MESSAGE_IX_CI_PW']
 
 url = 'https://data.ene.iiasa.ac.at/continuous_integration/' +\
       'scenario_db/db.tar.gz'
 filename = os.path.basename(url)
-
-print(filename)
 
 r = requests.get(url, auth=(username, password))
 
@@ -26,3 +28,9 @@ if r.status_code == 200:
 else:
     raise IOError(
         'Failed download with user/pass: {}/{}'.format(username, password))
+
+
+mp = ixmp.Platform(dbpath, dbtype='HSQLDB')
+print()
+print('Currently available scenarios:')
+print(mp.scenario_list())
